ab97b1ece6f2d769dddbcb92d50f4cd67840e529
Vagul <vaagul@gmail.com>
2022-02-10 14:31:54 +0530

    Textual changes

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ if __name__ == '__main__':
     orchestrator = Orchestrator()
     while True:
         value = input("Enter choice: \n1. Load all possible answers\n"
-                      "2. Add filter\n3. Display master list length\n"
-                      "4. Display master list\n5. Recommend word\n")
+                      "2. Add filter\n3. Display possible word list length\n"
+                      "4. Display possible word list\n5. Recommend word\n")
         if value == "0":
             break
         orchestrator.handle_selection(value)
